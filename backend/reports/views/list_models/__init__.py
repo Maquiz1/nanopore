85a047ee4c684e140b30eaf75426f53cf2420a4c
Maquiz1 <manquiz92@gmail.com>
2025-10-17 11:11:02 +0300

    fix all

--- backend/reports/views/list_models/__init__.py
+++ backend/reports/views/list_models/__init__.py
@@ -1,11 +1,11 @@
-from .download_models import ExportModelRawDataView
 from .list_models import list_models_view
-from .download_labels import DownloadModelLabelsView  # To avoid name clash
-from .download_fields import DownloadModelFieldsView  # To avoid name clash
+from .download_raw_data import ExportModelRawDataView
+from .download_labels_only import DownloadModelLabelsView  # To avoid name clash
+from .download_fields_only import DownloadModelFieldsView  # To avoid name clash
 
 __all__ = [
     "list_models_view",
     "ExportModelRawDataView",
-    "DownloadModelFieldsView",
     "DownloadModelLabelsView",
+    "DownloadModelFieldsView",
 ]
