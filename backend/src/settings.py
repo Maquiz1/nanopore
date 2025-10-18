2106eb12c6af8e677434964c2b930c630c9c05bf
Maquiz1 <manquiz92@gmail.com>
2025-10-18 20:50:55 +0300

    fix all

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -127,7 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ✅ Session Settings
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True      # Log out user when browser closes
-SESSION_COOKIE_AGE = 10                     # Session timeout (in seconds) → 10 seconds
+SESSION_COOKIE_AGE = 600                    # Session timeout (in seconds) → 10 seconds
 SESSION_SAVE_EVERY_REQUEST = True           # Extend session on each user activity
 SESSION_ENGINE = "django.contrib.sessions.backends.db"  # Store sessions in DB (default)
 
