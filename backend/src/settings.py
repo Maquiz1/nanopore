1cd853d3e6127707bef3a184a14290b450693825
Maquiz1 <manquiz92@gmail.com>
2025-10-19 11:07:41 +0300

    fix all

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -89,6 +89,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'reports.context_processors.global_report_total',  # Add this line
             ],
         },
     },
