44495cfd7e74afe98a377facc689761205d13c38
Maquiz1 <manquiz92@gmail.com>
2026-02-02 15:07:08 +0300

    Fix All

--- backend/static/custom_js/culture_isolate_fileds.js
+++ backend/static/custom_js/culture_isolate_fileds.js
@@ -1,29 +1,22 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Result fields
-//     const LjResults = document.getElementById("id_lj_results");
-//     const MgitResults = document.getElementById("id_mgit_results");
+document.addEventListener("DOMContentLoaded", function () {
+    // Result fields
+    const CultureIsolate = document.getElementById("id_culture_isolate");
 
-//     // Field to show/hide
-//     const IsolateHeader = document.getElementById("isolate-header");
-//     const cultureIsolate = document.getElementById("culture-isolate");
-//     const IsolateDate = document.getElementById("isolate-date");
+    // Field to show/hide
+    const IsolateDate = document.getElementById("isolate-date");
 
-//     function toggleCultureIsolate() {
-//         const ljValue = String(LjResults?.value || "");
-//         const mgitValue = String(MgitResults?.value || "");
+    function toggleCultureIsolate() {
+        const CultureIsolateValue = String(CultureIsolate?.value || "");
 
-//         // Show if LJ is 1-4 OR MGIT is 1
-//         const show = ["1", "2", "3", "4"].includes(ljValue) || mgitValue === "1";
+        // Show if LJ is 1-4 OR MGIT is 1
+        const show = ["1"].includes(CultureIsolateValue);
 
-//         IsolateHeader.style.display = show ? "block" : "none";
-//         cultureIsolate.style.display = show ? "block" : "none";
-//         IsolateDate.style.display = show ? "block" : "none";
-//     }
+        IsolateDate.style.display = show ? "block" : "none";
+    }
 
-//     // Initial state on page load
-//     toggleCultureIsolate();
+    // Initial state on page load
+    toggleCultureIsolate();
 
-//     // Update when either result changes
-//     LjResults.addEventListener("change", toggleCultureIsolate);
-//     MgitResults.addEventListener("change", toggleCultureIsolate);
-// });
+    // Update when either result changes
+    CultureIsolate.addEventListener("change", toggleCultureIsolate);
+});
