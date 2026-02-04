f3c5be9221c76dbcfc4bb37f0075ab6c1d1aa813
Maquiz1 <manquiz92@gmail.com>
2026-02-04 11:28:21 +0300

    Fix All

--- backend/locations/admin.py
+++ backend/locations/admin.py
@@ -26,7 +26,7 @@ class ZoneInline(admin.TabularInline):
 
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ['name','target']
+    list_display = ['name','target','substudy2Target','substudy4Target']
     inlines = [ZoneInline]
 
 @admin.register(Zone)
