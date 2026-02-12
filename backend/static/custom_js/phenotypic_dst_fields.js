4f80800f67ab0edcba7453e7bb0f21b3e9bfea6d
Maquiz1 <manquiz92@gmail.com>
2026-02-13 00:54:57 +0300

    fix all

--- backend/static/custom_js/phenotypic_dst_fields.js
+++ backend/static/custom_js/phenotypic_dst_fields.js
@@ -3,24 +3,27 @@ document.addEventListener("DOMContentLoaded", function () {
     const PhenotypicPerformed = document.getElementById("id_phenotypic_performed");
 
     // Field to show/hide
-    const phenotypicDatePerformed = document.getElementById("phenotypic-date-performed");
-
-    const phenotypicDstResultsHeader = document.getElementById("phenotypic-dst-results-header");
-    const phenotypicDateResults = document.getElementById("phenotypic-date-results");
-    const phenotypicDstResults = document.getElementById("phenotypic-dst-results");
-
     const isolateUniqueLabNo = document.getElementById("isolate-unique-lab-no");
 
-    const phenotypicDstResultsFirstLineHeader = document.getElementById("phenotypic-dst-results-first-line-header");
+    const phenotypicDatePerformed = document.getElementById("phenotypic-date-performed");
+    const phenotypicDateResults = document.getElementById("phenotypic-date-results");
+
     const firstLineDstPerformedDate = document.getElementById("first-line-dst-performed-date");
     const firstLineDstResultsDate = document.getElementById("first-line-dst-results-date");
 
-    const phenotypicDstResultsSecondLineHeader = document.getElementById("phenotypic-dst-results-second-line-header");
     const secondLineDstPerformedDate = document.getElementById("second-line-dst-performed-date");
     const secondLineDstResultsDate = document.getElementById("second-line-dst-results-date");
 
-    const phenotypicDstResultsOtherHeader = document.getElementById("phenotypic-dst-results-other-header");
-    const phenotypicDstResultsOther = document.getElementById("phenotypic-dst-results-other");
+    // const phenotypicDstResultsHeader = document.getElementById("phenotypic-dst-results-header");
+    // const phenotypicDstResults = document.getElementById("phenotypic-dst-results");
+
+    // const phenotypicDstResultsFirstLineHeader = document.getElementById("phenotypic-dst-results-first-line-header");
+
+
+    // const phenotypicDstResultsSecondLineHeader = document.getElementById("phenotypic-dst-results-second-line-header");
+
+    // const phenotypicDstResultsOtherHeader = document.getElementById("phenotypic-dst-results-other-header");
+    // const phenotypicDstResultsOther = document.getElementById("phenotypic-dst-results-other");
 
 
     // Function to toggle visibility
@@ -29,40 +32,49 @@ document.addEventListener("DOMContentLoaded", function () {
         const value = String(PhenotypicPerformed?.value || "");
 
         if (value === "1") {
-            phenotypicDstResultsHeader.style.display = "block";
+            isolateUniqueLabNo.style.display = "block";
+
             phenotypicDatePerformed.style.display = "block";
             phenotypicDateResults.style.display = "block";
 
-            phenotypicDstResults.style.display = "block";
-
-            isolateUniqueLabNo.style.display = "block";
-            phenotypicDstResultsFirstLineHeader.style.display = "block";
             firstLineDstPerformedDate.style.display = "block";
             firstLineDstResultsDate.style.display = "block";
 
-            phenotypicDstResultsSecondLineHeader.style.display = "block";
             secondLineDstPerformedDate.style.display = "block";
             secondLineDstResultsDate.style.display = "block";
 
-            phenotypicDstResultsOtherHeader.style.display = "block";
-            phenotypicDstResultsOther.style.display = "block";
+            // phenotypicDstResultsHeader.style.display = "block";
+            // phenotypicDstResults.style.display = "block";
+
+            // phenotypicDstResultsFirstLineHeader.style.display = "block";
+
+
+            // phenotypicDstResultsSecondLineHeader.style.display = "block";
+
+
+            // phenotypicDstResultsOtherHeader.style.display = "block";
+            // phenotypicDstResultsOther.style.display = "block";
         } else {
+            isolateUniqueLabNo.style.display = "none";
+
             phenotypicDatePerformed.style.display = "none";
             phenotypicDateResults.style.display = "none";
-            phenotypicDstResultsHeader.style.display = "none";
 
-            phenotypicDstResults.style.display = "none";
-
-            isolateUniqueLabNo.style.display = "none";
-            phenotypicDstResultsFirstLineHeader.style.display = "none";
             firstLineDstPerformedDate.style.display = "none";
             firstLineDstResultsDate.style.display = "none";
-            phenotypicDstResultsSecondLineHeader.style.display = "none";
+
             secondLineDstPerformedDate.style.display = "none";
             secondLineDstResultsDate.style.display = "none";
 
-            phenotypicDstResultsOtherHeader.style.display = "none";
-            phenotypicDstResultsOther.style.display = "none";
+            // phenotypicDstResultsHeader.style.display = "none";
+
+            // phenotypicDstResults.style.display = "none";
+
+            // phenotypicDstResultsFirstLineHeader.style.display = "none";
+            // phenotypicDstResultsSecondLineHeader.style.display = "none";
+
+            // phenotypicDstResultsOtherHeader.style.display = "none";
+            // phenotypicDstResultsOther.style.display = "none";
         }
     }
 
