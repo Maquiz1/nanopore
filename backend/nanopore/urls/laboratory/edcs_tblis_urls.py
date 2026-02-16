0b38e2375fb827cff86fc2a29c6d0a0cbfdebd84
Maquiz1 <manquiz92@gmail.com>
2026-02-17 01:17:47 +0300

    fix all

--- backend/nanopore/urls/laboratory/edcs_tblis_urls.py
+++ backend/nanopore/urls/laboratory/edcs_tblis_urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path("edcs-tblis-list/", EdcsTBLISLaboratoryListView.as_view(), name="edcs-tblis-laboratory-list"),
     # path("zonal-lab/<int:pk>/", ZonalLaboratoryDetailView.as_view(), name="zonal-laboratory-detail"),
     # path("zonal-lab/form/", ZonalLabFormView.as_view(), name="zonal-laboratory-create"),
-    # path("zonal-lab/form/<int:pk>/", ZonalLabFormView.as_view(), name="zonal-laboratory-update"),
-    path("edcs-tblis/form/<int:pk>/", EdcsTblisFormView.as_view(), name="edcs-tblis-laboratory-view"),
+    path("edcs-tblis/form/<int:pk>/update", EdcsTblisFormView.as_view(), name="edcs-tblis-laboratory-update"),
+    path("edcs-tblis/form/<int:pk>/view", EdcsTblisFormView.as_view(), name="edcs-tblis-laboratory-view"),
     # path("zonal-lab/<int:pk>/delete/", ZonalLaboratoryDeleteView.as_view(), name="zonal-laboratory-delete"),
     
     path('upload-edcs-tblis/', EdcsTBLISCsvUploadView.as_view(), name="edcs-tblis-upload-csv"),
