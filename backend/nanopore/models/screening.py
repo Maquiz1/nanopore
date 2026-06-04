c0db318ca61d6d65f8ae71c78e40cfbc1e63eaf6
Maquiz1 <manquiz92@gmail.com>
2026-06-04 05:54:29 +0300

    fix

--- backend/nanopore/models/screening.py
+++ backend/nanopore/models/screening.py
@@ -45,6 +45,7 @@ class Screening(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name="screenings_updated")
     test2 = models.CharField(blank=True, null=True)
+    test3 = models.CharField(blank=True, null=True)
 
     class Meta:
         ordering = ["screening_date", "pid"]
